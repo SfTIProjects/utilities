6d4ff83c1cd6147284221b0fb090deb8b683b38c
Ifeanyi Ndukwe glory.ndukwe@otago.ac.nz <nduif39p@mahuika01.mahuika.nesi.org.nz>
2022-02-11 20:01:17 +1300

    Anyi@@2

---
+++ init_pmd_root_folder.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+module load Python/3.9.5-gimkl-2020a
+
+python init_root_folder_4_sta_code_ana.py -sld libs -slfp pmdrules.xml -eslf -slfrp pmd-bin-* -eslfr -rn pmd -rd ../my_codesnippet_analysis -ucrd -dln libs -an mvn_apps -xn xmlreports -ecn errorcodeanalysis -ccn completecodeanalysis 
